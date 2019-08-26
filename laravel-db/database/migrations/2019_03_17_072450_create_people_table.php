eb4839977be37925b04f6271224e040e0fb0725c
Hanan <48240920+hanandw@users.noreply.github.com>
2019-08-26 10:07:59 +0700

    update kode tulisan: Membuat Model Aplikasi

---
+++ laravel-db/database/migrations/2019_03_17_072450_create_people_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('city_id');
+            $table->string('name');
+            $table->string('age');
+            $table->timestamps();
+            $table->foreign('city_id')->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
